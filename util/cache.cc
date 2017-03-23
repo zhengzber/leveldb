0f113aa516ecbad1ed43ee540b31f18d5773a181
zzkeln <zhengzhibin90@gmail.com>
2017-03-23 18:56:31 +0800

    Update cache.cc

--- util/cache.cc
+++ util/cache.cc
@@ -40,12 +40,15 @@ namespace {
 // An entry is a variable length heap-allocated structure.  Entries
 // are kept in a circular doubly linked list ordered by access time.
  
- 
+//一个节点如果在LRUcache中，要么在lru双向链表中，要么在in_use双向链表中（此时被client所使用），肯定会在hash table中。
+//如果在lru双向链表中，那么ref=1并且in_hash=true
+//如果在in_use双向链表中，那么ref>=2（因为此时至少为一个client所使用）并且in_hash=true
+//(之前dirtysalt分析的LRUCache没有in_use双向链表的概念，仅仅有lru双向链表)
   
 struct LRUHandle {
   void* value; //键值对的值
   void (*deleter)(const Slice&, void* value); //这个结构体的清除函数，由外界传进去注册
-  LRUHandle* next_hash; //用于hash表冲突时使用，在哈希表中，指向哈希数组的链表中的下一个节点指针
+  LRUHandle* next_hash; //用于hash表冲突时使用，在哈希表中，指向哈希数组的链表中的下一个节点指针。哈希数组冲突的拉链是单向链表
   LRUHandle* next; //当前节点的下一个节点
   LRUHandle* prev; //当前节点的上一个节点
   size_t charge;      // TODO(opt): Only allow uint32_t? 这个节点占用的内存大小
@@ -83,12 +86,11 @@ class HandleTable {
     return *FindPointer(key, hash);
   }
 
-  //如果ptr不为空，那么h放在ptr的后面；如果为空，那么h放在*ptr的位置
-  //插入操作,先查找要插入的键值是否在哈希表中，如果在，那么用新的节点替换就的节点，并且函数返回旧的节点。
+  //h将会替换*ptr的位置，返回旧的节点（该节点会从哈希表中删除）
   LRUHandle* Insert(LRUHandle* h) {
     LRUHandle** ptr = FindPointer(h->key(), h->hash); //查找要插入节点的位置
     LRUHandle* old = *ptr;
-    //将新节点插入哈希表中
+    //将h替换原来节点的位置
     h->next_hash = (old == NULL ? NULL : old->next_hash);
     *ptr = h;
     if (old == NULL) {
@@ -125,7 +127,8 @@ class HandleTable {
   // Return a pointer to slot that points to a cache entry that
   // matches key/hash.  If there is no such cache entry, return a
   // pointer to the trailing slot in the corresponding linked list.
-  //在哈希表中，根据key和hash来找到LRUHandle*。找到一个节点它的key等参数key，或者hash等于参数hash然后返回
+  //在哈希表中，根据key和hash来找到LRUHandle*。
+  //找到一个非空节点，并且该接的hash值和key都等于参数，返回这个节点的地址的地址
   //如果没找到的话，那么返回的是*ptr=NULL,这个ptr指向的是哈希数组的slot链表中的最后一个节点的下个NULL节点，可作为insert节点使用
   LRUHandle** FindPointer(const Slice& key, uint32_t hash) {
     LRUHandle** ptr = &list_[hash & (length_ - 1)];
@@ -217,13 +220,14 @@ class LRUCache {
   // Entries are in use by clients, and have refs >= 2 and in_cache==true.
   LRUHandle in_use_; //in-user链表的头节点
   
-  HandleTable table_; //上面的哈希表，一个双向链表还附带一个哈希表
+  HandleTable table_; //上面的哈希表，一个LRUCache附带一个哈希表。如果一个LRUHandle在此LRUCache中，不管在lru还是in_use双向链表中都肯定在hash table中
+  // hash table为了方便查找了，可以快速找在hash table中找到LRUHandle*
 };
 
 LRUCache::LRUCache()
     : usage_(0) {
   // Make empty circular linked lists.
-  //空的循环链表，头节点指向自己
+  //初始lru和in_use双向链表都是空的，头节点指向自己
   lru_.next = &lru_;
   lru_.prev = &lru_;
   in_use_.next = &in_use_;
@@ -231,7 +235,9 @@ LRUCache::LRUCache()
 }
 
 LRUCache::~LRUCache() {
+  //此时in_use双向链表是空的，已经没用LRUHandle被client所使用
   assert(in_use_.next == &in_use_);  // Error if caller has an unreleased handle
+  //遍历lru双向链表，把每个节点的in_cache设置成false,然后unref下（会调用节点内部的释放函数来释放掉该节点）
   for (LRUHandle* e = lru_.next; e != &lru_; ) {
     LRUHandle* next = e->next;
     assert(e->in_cache);
@@ -242,6 +248,7 @@ LRUCache::~LRUCache() {
   }
 }
 
+//会增加引用计数，如果在对lru双向链表的节点进行ref，那么会从lru双向链表中移除，然后放入in_use双向链表中
 void LRUCache::Ref(LRUHandle* e) {
   //从lru链表中移除，放入in_use链表中
   if (e->refs == 1 && e->in_cache) {  // If on lru_ list, move to in_use_ list.
@@ -251,28 +258,32 @@ void LRUCache::Ref(LRUHandle* e) {
   e->refs++;
 }
 
+//减少引用计数，如果ref=0那么删除该节点。如果ref=1 &in_cache=1那么将e从in_use双向链表中移除并加入lru双向链表中。
+//不会加锁，由外部加锁保证
 void LRUCache::Unref(LRUHandle* e) {
   assert(e->refs > 0);
   e->refs--;
   //如果当前引用计数为0，那么调用注册的释放函数来删除节点
+  //走到这个逻辑是从lru双向链表的节点进行unref
   if (e->refs == 0) { // Deallocate.
     assert(!e->in_cache);
     (*e->deleter)(e->key(), e->value);
     free(e);
   } else if (e->in_cache && e->refs == 1) {  // No longer in use; move to lru_ list.
+    //走到这个逻辑一般是从in_use双向链表的节点进行unref
     //解除e节点在双向链表中的位置，放入lru链表中
     LRU_Remove(e);
     LRU_Append(&lru_, e);
   }
 }
 
-//把这个节点从双向链表中解除关系，不负责释放e节点
+//把这个节点从当前双向链表中移除
 void LRUCache::LRU_Remove(LRUHandle* e) {
   e->next->prev = e->prev;
   e->prev->next = e->next;
 }
 
-//将e节点插入到ist节点前面
+//将e节点插入到list的双向链表中，插入到list头节点的prev中
 void LRUCache::LRU_Append(LRUHandle* list, LRUHandle* e) {
   // Make "e" newest entry by inserting just before *list
   e->next = list;
@@ -281,7 +292,8 @@ void LRUCache::LRU_Append(LRUHandle* list, LRUHandle* e) {
   e->next->prev = e;
 }
 
-//如果从哈希表中找到该节点，那么增加他的引用计数（会从lru中移除，放入in_use中），返回该节点
+//从哈希表中找该节点，如果找到的话那么ref(e)（如果此时在lru节点，那么会挪到in_use节点，并且ref++；
+//如果已经在in_use节点那么仅仅ref++；因为会被client使用），然后返回该节点
 Cache::Handle* LRUCache::Lookup(const Slice& key, uint32_t hash) {
   MutexLock l(&mutex_);
   LRUHandle* e = table_.Lookup(key, hash);
@@ -291,12 +303,13 @@ Cache::Handle* LRUCache::Lookup(const Slice& key, uint32_t hash) {
   return reinterpret_cast<Cache::Handle*>(e);
 }
   
-//解引用该节点（引用计数会--）
+//加锁，然后unref节点（可能涉及在lru或in_use之间的腾挪）
 void LRUCache::Release(Cache::Handle* handle) {
   MutexLock l(&mutex_);
   Unref(reinterpret_cast<LRUHandle*>(handle));
 }
 
+//插入节点，加锁保证
 Cache::Handle* LRUCache::Insert(
     const Slice& key, uint32_t hash, void* value, size_t charge,
     void (*deleter)(const Slice& key, void* value)) {
@@ -309,26 +322,27 @@ Cache::Handle* LRUCache::Insert(
   e->deleter = deleter; //删除函数
   e->charge = charge; //占用大小
   e->key_length = key.size(); //key
-  e->hash = hash; // hash
-  e->in_cache = false;
-  e->refs = 1;  // for the returned handle.
-  memcpy(e->key_data, key.data(), key.size());
-
+  e->hash = hash; // hash值
+  e->in_cache = false; //初始不在hash表中
+  e->refs = 1;  // for the returned handle.由于要返回一个句柄因此先设置为1
+  memcpy(e->key_data, key.data(), key.size());//将key挪到节点的内存中
+ 
+  //如果当前存在capacity，放到hash table和in_use双向链表中（因为会返回一个句柄因此放入in_use中）
   if (capacity_ > 0) {
     e->refs++;  // for the cache's reference.
     e->in_cache = true;
     //放入in_use链表中
     LRU_Append(&in_use_, e);
     usage_ += charge;
-    FinishErase(table_.Insert(e));
+    FinishErase(table_.Insert(e)); //插入到hash table中，返回的旧节点会unref
   } // else don't cache.  (Tests use capacity_==0 to turn off caching.)
 
-  //淘汰出一个
-  //从lru节点中淘汰一个
+
+  //从lru链表中移除旧节点(lru->next指向的节点)，同时从hash table中移除
   while (usage_ > capacity_ && lru_.next != &lru_) {
     LRUHandle* old = lru_.next;
     assert(old->refs == 1);
-    bool erased = FinishErase(table_.Remove(old->key(), old->hash));
+    bool erased = FinishErase(table_.Remove(old->key(), old->hash));//从hash table中移除，也会unref
     if (!erased) {  // to avoid unused variable when compiled NDEBUG
       assert(erased);
     }
@@ -339,6 +353,7 @@ Cache::Handle* LRUCache::Insert(
 
 // If e != NULL, finish removing *e from the cache; it has already been removed
 // from the hash table.  Return whether e != NULL.  Requires mutex_ held.
+//从双向链表中移除，设置不在hash table中，同时unref节点
 bool LRUCache::FinishErase(LRUHandle* e) {
   if (e != NULL) {
     assert(e->in_cache);
@@ -350,11 +365,13 @@ bool LRUCache::FinishErase(LRUHandle* e) {
   return e != NULL;
 }
 
+//从hash table中移除该节点，并且unref
 void LRUCache::Erase(const Slice& key, uint32_t hash) {
   MutexLock l(&mutex_);
   FinishErase(table_.Remove(key, hash));
 }
 
+//把lru中的节点都移除，也会从hash table中移除，最后会释放这些节点
 void LRUCache::Prune() {
   MutexLock l(&mutex_);
   while (lru_.next != &lru_) {
