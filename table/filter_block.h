9a5187f46a678d157ff81d9432cb0381c1ffbd52
zzkeln <zhengzhibin90@gmail.com>
2017-03-26 15:06:36 +0800

    Update filter_block.h

--- table/filter_block.h
+++ table/filter_block.h
@@ -27,6 +27,7 @@ class FilterPolicy;
 // The sequence of calls to FilterBlockBuilder must match the regexp:
 //      (StartBlock AddKey*)* Finish
 //meta block存放了bloom filter信息，这样可以减少磁盘读取
+//构建一个filter block,包括filter1...filtern, filter1偏移....filtern偏移，filter偏移首地址
 class FilterBlockBuilder {
  public:
   explicit FilterBlockBuilder(const FilterPolicy*);
@@ -37,12 +38,16 @@ class FilterBlockBuilder {
 
  private:
   void GenerateFilter();
-
+ 
+  //其实key_和start_都是为了AddKey而存在的，添加key时，会把key追加到key_后面并把相应偏移放入start_中，生成filter时会从key_和start_数组
+  //复原出来各个key并放入tmp_keys_中。其实不用key_和start_也可以，Addkey时直接将key添加到tmp_keys_中也可以，这样就不用复原了，可能潜在的
+  //的问题是vector需要不断重新分配内存！
+ 
   const FilterPolicy* policy_;    //过滤策略，比较有名的是布隆过滤器
-  std::string keys_;              // Flattened key contents 每一个filter条目包含的键值
-  std::vector<size_t> start_;     // Starting index in keys_ of each key 每个Filter条目包含键值的首地址偏移量，相对于keys_首地址来说
-  std::string result_;            // Filter data computed so far 到目前为止，所有Filter条目包含的数据
-  std::vector<Slice> tmp_keys_;   // policy_->CreateFilter() argument 临时slice向量，用于向result_添加本次keys_数据
+  std::string keys_;              // Flattened key contents：当前filter包含的键值
+  std::vector<size_t> start_;     // Starting index in keys_ of each key：当前Filter包含键值的首地址偏移量，相对于keys_首地址来说
+  std::string result_;            // Filter data computed so far: 目前为止filter block构建出的内容
+  std::vector<Slice> tmp_keys_;   // policy_->CreateFilter() argument： 临时slice向量，用于向result_添加本次keys_数据
   std::vector<uint32_t> filter_offsets_; //每个filter的偏移量
  
   // No copying allowed
