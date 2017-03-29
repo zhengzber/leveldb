62abc291c6dc7d7941bc904245d49d14dea2170f
zzkeln <zhengzhibin90@gmail.com>
2017-03-29 14:47:14 +0800

    Update table_builder.h

--- include/leveldb/table_builder.h
+++ include/leveldb/table_builder.h
@@ -23,12 +23,13 @@ class BlockBuilder;
 class BlockHandle;
 class WritableFile;
 
+//TableBuilder用来帮助构建Table.而Table本身只是用来进行查询遍历等操作.用户在完成之后需要Finish或者是Abandon 
 class TableBuilder {
  public:
   // Create a builder that will store the contents of the table it is
   // building in *file.  Does not close the file.  It is up to the
   // caller to close the file after calling Finish().
-  TableBuilder(const Options& options, WritableFile* file);
+  TableBuilder(const Options& options, WritableFile* file); //传入options和可写文件(追加写模式)
 
   // REQUIRES: Either Finish() or Abandon() has been called.
   ~TableBuilder();
@@ -39,18 +40,18 @@ class TableBuilder {
   // passed to the constructor is different from its value in the
   // structure passed to this method, this method will return an error
   // without changing any fields.
-  Status ChangeOptions(const Options& options);
+  Status ChangeOptions(const Options& options); // 一旦构建好之后只允许修改部分字段.
 
   // Add key,value to the table being constructed.
   // REQUIRES: key is after any previously added key according to comparator.
   // REQUIRES: Finish(), Abandon() have not been called
-  void Add(const Slice& key, const Slice& value);
+  void Add(const Slice& key, const Slice& value); // 添加kv
 
   // Advanced operation: flush any buffered key/value pairs to file.
   // Can be used to ensure that two adjacent entries never live in
   // the same data block.  Most clients should not need to use this method.
   // REQUIRES: Finish(), Abandon() have not been called
-  void Flush();
+  void Flush(); // 将buffered的kv刷新到文件
 
   // Return non-ok iff some error has been detected.
   Status status() const;
@@ -58,21 +59,21 @@ class TableBuilder {
   // Finish building the table.  Stops using the file passed to the
   // constructor after this function returns.
   // REQUIRES: Finish(), Abandon() have not been called
-  Status Finish();
-
+  Status Finish(); //构建完成
+ 
   // Indicate that the contents of this builder should be abandoned.  Stops
   // using the file passed to the constructor after this function returns.
   // If the caller is not going to call Finish(), it must call Abandon()
   // before destroying this builder.
   // REQUIRES: Finish(), Abandon() have not been called
-  void Abandon();
-
+  void Abandon(); //放弃构建
+ 
   // Number of calls to Add() so far.
-  uint64_t NumEntries() const;
-
+  uint64_t NumEntries() const;//添加了多少次kv
+ 
   // Size of the file generated so far.  If invoked after a successful
   // Finish() call, returns the size of the final generated file.
-  uint64_t FileSize() const;
+  uint64_t FileSize() const; //当前已经写入多少文件了.
 
  private:
   bool ok() const { return status().ok(); }
