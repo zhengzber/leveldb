c5f2a7219b2395977b3e0edcb1cb9cc03c86d2a1
zzkeln <zhengzhibin90@gmail.com>
2017-03-29 19:38:48 +0800

    Update block.h

--- table/block.h
+++ table/block.h
@@ -14,6 +14,7 @@ namespace leveldb {
 struct BlockContents;
 class Comparator;
 
+//用于读取data block
 class Block {
  public:
   // Initialize the block with the specified contents.
@@ -22,15 +23,15 @@ class Block {
   ~Block();
 
   size_t size() const { return size_; }
-  Iterator* NewIterator(const Comparator* comparator);
+  Iterator* NewIterator(const Comparator* comparator); //返回这个block的迭代器
 
  private:
   uint32_t NumRestarts() const;
 
-  const char* data_;
-  size_t size_;
-  uint32_t restart_offset_;     // Offset in data_ of restart array
-  bool owned_;                  // Block owns data_[]
+  const char* data_; //指向data block的首地址
+  size_t size_; // data block的数据大小
+  uint32_t restart_offset_;     // Offset in data_ of restart array // 重启点数组的起始地址，每个restart都是4个字节
+  bool owned_;                  // Block owns data_[]  //是否要用户自己delete这个block
 
   // No copying allowed
   Block(const Block&);
