298b1f5b92d3fcbf676eef2dde2bdd7128041037
zzkeln <zhengzhibin90@gmail.com>
2017-03-30 15:48:05 +0800

    Update snapshot.h

--- db/snapshot.h
+++ db/snapshot.h
@@ -12,33 +12,45 @@ namespace leveldb {
 
 class SnapshotList;
 
+/*
+Snapshot实现非常简单，就是一个双向链表的节点，然后挂在一个双向链表上面。 
+每一个Snapshot实现都附带一个seq number.对于Snapshot最重要的应该是在上面的操作吧. 
+我们可以猜想对于每次插入的key都会带上一个seq number.这样如果对snapshot操作的话读取的话，
+那么只需要读取seq number以下的内容即可了。 
+*/ 
+ 
 // Snapshots are kept in a doubly-linked list in the DB.
 // Each SnapshotImpl corresponds to a particular sequence number.
+//每个SnapshotImpl是一个节点
 class SnapshotImpl : public Snapshot {
  public:
-  SequenceNumber number_;  // const after creation
-
+  SequenceNumber number_;  // const after creation 当前快照的序列号 
+ 
  private:
   friend class SnapshotList;
 
   // SnapshotImpl is kept in a doubly-linked circular list
+ // 用于插入链表时，更新前后关系
   SnapshotImpl* prev_;
   SnapshotImpl* next_;
 
-  SnapshotList* list_;                 // just for sanity checks
+  SnapshotList* list_;                 // just for sanity checks //这个节点所属的链表，源码注释是“合理性检查”
 };
 
+//双向链表
 class SnapshotList {
  public:
   SnapshotList() {
+   //初始dummy节点时，前后节点为自己
     list_.prev_ = &list_;
     list_.next_ = &list_;
   }
 
-  bool empty() const { return list_.next_ == &list_; }
-  SnapshotImpl* oldest() const { assert(!empty()); return list_.next_; }
-  SnapshotImpl* newest() const { assert(!empty()); return list_.prev_; }
+  bool empty() const { return list_.next_ == &list_; } //判断是否为空
+  SnapshotImpl* oldest() const { assert(!empty()); return list_.next_; }//取出最“老”的快照
+  SnapshotImpl* newest() const { assert(!empty()); return list_.prev_; }//取出最“新”的快照
 
+  ////新生成一个快照，并插入链表中
   const SnapshotImpl* New(SequenceNumber seq) {
     SnapshotImpl* s = new SnapshotImpl;
     s->number_ = seq;
@@ -50,6 +62,7 @@ class SnapshotList {
     return s;
   }
 
+  ////删除一个快照
   void Delete(const SnapshotImpl* s) {
     assert(s->list_ == this);
     s->prev_->next_ = s->next_;
