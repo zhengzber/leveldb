24147342597f50c702d1ff7739d1c884a3cd9668
zzkeln <zhengzhibin90@gmail.com>
2017-05-28 16:11:49 +0800

    Update status.h

--- include/leveldb/status.h
+++ include/leveldb/status.h
@@ -18,18 +18,22 @@
 
 namespace leveldb {
 
+//ErrCode和ErrMsg的封装，都用一个字符数组来表示。整体用类来封装。
+//如果state_是NULL表示状态ok，其它情况需要分配内存到state_中来表示异常情况：这样的好处是如果状态大多数是ok的话，那么不需要从堆中分配任何内存了
+//只在异常状态下才会需要从堆中分配内存，可以节省一点内存申请。
 class Status {
  public:
+  //注意stata_是NULL的时候，表示Status是ok，即状态正常。其它情况都表示状态异常，异常code是第5个字节，异常msg是第6个字节开始的字符串。
   // Create a success status.
-  Status() : state_(NULL) { }
-  ~Status() { delete[] state_; }
+  Status() : state_(NULL) { } //默认构造函数创建一个空的Status，字符串为NULL，表示状态是ok
+  ~Status() { delete[] state_; } //析构函数删除字符串内存，所以一般的构造函数要分配内存
 
   // Copy the specified status.
   Status(const Status& s);
   void operator=(const Status& s);
 
   // Return a success status.
-  static Status OK() { return Status(); }
+  static Status OK() { return Status(); } //返回一个OK的status，就是默认构造函数即可。
 
   // Return error status of an appropriate type.
   static Status NotFound(const Slice& msg, const Slice& msg2 = Slice()) {
@@ -49,7 +53,8 @@ class Status {
   }
 
   // Returns true iff the status indicates success.
-  bool ok() const { return (state_ == NULL); }
+ //state_==NULL时表示everything is ok
+  bool ok() const { return (state_ == NULL); } //
 
   // Returns true iff the status indicates a NotFound error.
   bool IsNotFound() const { return code() == kNotFound; }
@@ -76,7 +81,8 @@ class Status {
   //    state_[0..3] == length of message
   //    state_[4]    == code
   //    state_[5..]  == message
-  const char* state_;
+  const char* state_; //前4个字节是state的err_msg的长度，第5个字节表示错误码Code，第6个字节开始是err_msg。所以整体state_的长度是前4个字节
+ //所表示的长度+5字节
 
   enum Code {
     kOk = 0,
@@ -87,23 +93,31 @@ class Status {
     kIOError = 5
   };
 
+ //返回状态码，如果state_是NULL就返回ok，否则返回4个字节（将字节转换成Code）。
   Code code() const {
     return (state_ == NULL) ? kOk : static_cast<Code>(state_[4]);
   }
 
+  //这也是一个构造函数，状态吗为code， err_msg为msg和msg2的err_msg的拼接（中间用：和空格来分隔），需要分配内存。
+  //一般err_msg都较小，不然得话在构造函数中分配内存不太好。
   Status(Code code, const Slice& msg, const Slice& msg2);
   static const char* CopyState(const char* s);
 };
 
+//复制构造函数，如果s.state_==NULL那么当前state_也设置为NULL，都表示状态ok。
+//否则分配一块内存，来放置s的state_
 inline Status::Status(const Status& s) {
   state_ = (s.state_ == NULL) ? NULL : CopyState(s.state_);
 }
+//赋值构造函数
 inline void Status::operator=(const Status& s) {
   // The following condition catches both aliasing (when this == &s),
   // and the common case where both s and *this are ok.
+  //先判断2个state_是否相等（如果都是NULL那么也会match this condition)。
   if (state_ != s.state_) {
+    //否则，删除本state_的内存，然后将s.state_的内存拷贝到state_
     delete[] state_;
-    state_ = (s.state_ == NULL) ? NULL : CopyState(s.state_);
+    state_ = (s.state_ == NULL) ? NULL : CopyState(s.state_); //这里还是需要判断s.state是否为NULL的，如果为NULL就不用分配内存拉。
   }
 }
 
