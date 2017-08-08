e0c291a64c7481c6fe065b89caa853b98c1eabb9
zzkeln <zhengzhibin90@gmail.com>
2017-08-08 12:02:44 +0800

    Update write_batch.h

--- include/leveldb/write_batch.h
+++ include/leveldb/write_batch.h
@@ -2,8 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file. See the AUTHORS file for names of contributors.
 //
-// WriteBatch holds a collection of updates to apply atomically to a DB.
 //
+// WriteBatch holds a collection of updates to apply atomically to a DB.
 // The updates are applied in the order in which they are added
 // to the WriteBatch.  For example, the value of "key" will be "v3"
 // after the following batch is written:
