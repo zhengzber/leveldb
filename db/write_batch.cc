7db14f783cedf9447531cc77b7bbd3573a36be1b
zzkeln <zhengzhibin90@gmail.com>
2017-08-08 12:02:40 +0800

    Update write_batch.cc

--- db/write_batch.cc
+++ db/write_batch.cc
@@ -1,7 +1,7 @@
 // Copyright (c) 2011 The LevelDB Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file. See the AUTHORS file for names of contributors.
-//
+//在12个字节之后，就是一条条的记录了。
 // WriteBatch::rep_ :=
 //    sequence: fixed64
 //    count: fixed32
@@ -34,26 +34,33 @@ WriteBatch::~WriteBatch() { }
 
 WriteBatch::Handler::~Handler() { }
 
+//留12字节大小作为头部
 void WriteBatch::Clear() {
   rep_.clear();
   rep_.resize(kHeader);
 }
 
+//参数是Handler,实现类是MemTableInsert*，包含put(key, value)和delete(key)接口，即将key,value放入memtable或从memtable中删除（其实
+//实现是添加一条删除记录，而不是真正的删除，只在compaction时才会去删除该记录）
+//这个方法解析出一条记录，然后判断记录的类型，根据不同的类型，
+//调用Hander不同的方法将记录插入memtable中。这个方法最后由WriteBatchInternal这个类的InsertInto调用；
 Status WriteBatch::Iterate(Handler* handler) const {
   Slice input(rep_);
-  if (input.size() < kHeader) {
+  if (input.size() < kHeader) { //至少大于等于12字节
     return Status::Corruption("malformed WriteBatch (too small)");
   }
 
+  //移除12字节头部
   input.remove_prefix(kHeader);
   Slice key, value;
   int found = 0;
   while (!input.empty()) {
     found++;
     char tag = input[0];
-    input.remove_prefix(1);
+    input.remove_prefix(1);//移除第一个字节，这个字节是类型
     switch (tag) {
       case kTypeValue:
+        //如果是key,value类型，拿出key, value然后调用handler的put接口放入memtable中
         if (GetLengthPrefixedSlice(&input, &key) &&
             GetLengthPrefixedSlice(&input, &value)) {
           handler->Put(key, value);
@@ -62,6 +69,7 @@ Status WriteBatch::Iterate(Handler* handler) const {
         }
         break;
       case kTypeDeletion:
+        //如果是delete类型，拿出key，调用handler的delete接口，将删除键类型加入memtable中
         if (GetLengthPrefixedSlice(&input, &key)) {
           handler->Delete(key);
         } else {
@@ -72,6 +80,7 @@ Status WriteBatch::Iterate(Handler* handler) const {
         return Status::Corruption("unknown WriteBatch tag");
     }
   }
+  //如果发现的记录数不等于这个writebatch里count字段即记录数字段，返回失败
   if (found != WriteBatchInternal::Count(this)) {
     return Status::Corruption("WriteBatch has wrong count");
   } else {
@@ -95,20 +104,23 @@ void WriteBatchInternal::SetSequence(WriteBatch* b, SequenceNumber seq) {
   EncodeFixed64(&b->rep_[0], seq);
 }
 
+//添加key, value
 void WriteBatch::Put(const Slice& key, const Slice& value) {
-  WriteBatchInternal::SetCount(this, WriteBatchInternal::Count(this) + 1);
-  rep_.push_back(static_cast<char>(kTypeValue));
-  PutLengthPrefixedSlice(&rep_, key);
-  PutLengthPrefixedSlice(&rep_, value);
+  WriteBatchInternal::SetCount(this, WriteBatchInternal::Count(this) + 1);//先增加记录数
+  rep_.push_back(static_cast<char>(kTypeValue));//添加key,value类型
+  PutLengthPrefixedSlice(&rep_, key);//添加key
+  PutLengthPrefixedSlice(&rep_, value);//添加value
 }
 
+//添加一条记录，为删除类型，key
 void WriteBatch::Delete(const Slice& key) {
-  WriteBatchInternal::SetCount(this, WriteBatchInternal::Count(this) + 1);
-  rep_.push_back(static_cast<char>(kTypeDeletion));
-  PutLengthPrefixedSlice(&rep_, key);
+  WriteBatchInternal::SetCount(this, WriteBatchInternal::Count(this) + 1);//新增加记录数
+  rep_.push_back(static_cast<char>(kTypeDeletion));//添加删除类型
+  PutLengthPrefixedSlice(&rep_, key);//添加key
 }
 
 namespace {
+//这个类就是将key,value放入memtable中
 class MemTableInserter : public WriteBatch::Handler {
  public:
   SequenceNumber sequence_;
@@ -125,6 +137,7 @@ class MemTableInserter : public WriteBatch::Handler {
 };
 }  // namespace
 
+//遍历writebatch的所有记录，然后根据记录类型调用MemtableInsert的不同接口，将记录添加到memtable中
 Status WriteBatchInternal::InsertInto(const WriteBatch* b,
                                       MemTable* memtable) {
   MemTableInserter inserter;
@@ -133,14 +146,16 @@ Status WriteBatchInternal::InsertInto(const WriteBatch* b,
   return b->Iterate(&inserter);
 }
 
+//设置writebatch的内容
 void WriteBatchInternal::SetContents(WriteBatch* b, const Slice& contents) {
   assert(contents.size() >= kHeader);
   b->rep_.assign(contents.data(), contents.size());
 }
-
+//将src中的所有记录添加进dst中
 void WriteBatchInternal::Append(WriteBatch* dst, const WriteBatch* src) {
-  SetCount(dst, Count(dst) + Count(src));
+  SetCount(dst, Count(dst) + Count(src));//设置dst的记录数位dst和src的记录数之和
   assert(src->rep_.size() >= kHeader);
+  //将src头部之后的记录数据全部append到dst中
   dst->rep_.append(src->rep_.data() + kHeader, src->rep_.size() - kHeader);
 }
 
