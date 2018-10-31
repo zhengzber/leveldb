b2d73f394139dfb6695160fb75885a7e197ad071
zzkeln <zhengzhibin90@gmail.com>
2018-10-31 10:38:35 +0800

    Update memtable.h

--- db/memtable.h
+++ db/memtable.h
@@ -18,6 +18,7 @@ class Mutex;
 class MemTableIterator;
 
 //memtable是代理模式，底层用的存储（放内存中）是skiplist
+// memtable的查询key是LookupKey
 class MemTable {
  public:
   // MemTables are reference counted.  The initial reference count
@@ -54,6 +55,7 @@ class MemTable {
   // Add an entry into memtable that maps key to value at the
   // specified sequence number and with the specified type.
   // Typically value will be empty if type==kTypeDeletion.
+ //增加一个entry: internal-key-size+internal-key+value-size+value
   void Add(SequenceNumber seq, ValueType type,
            const Slice& key,
            const Slice& value);
@@ -62,6 +64,7 @@ class MemTable {
   // If memtable contains a deletion for key, store a NotFound() error
   // in *status and return true.
   // Else, return false.
+  //获得key对应的value
   bool Get(const LookupKey& key, std::string* value, Status* s);
 
  private:
